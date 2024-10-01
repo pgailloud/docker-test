65e5cfd23fdad7399093a523415ea254cdee0ddd
Pascal Gailloud <pgailloud@bluewin.ch>
2024-10-01 08:49:12 +0200

    Modification gestion mot de passe

--- src/database.php
+++ src/database.php
@@ -1,17 +1,12 @@
 <?php
 
 error_reporting(E_ALL);
-echo "version 2";
+echo "version 3";
 // Read the database connection parameters from environment variables
 $db_host = getenv('DB_HOST');
 $db_name = getenv('DB_NAME');
 $db_user = getenv('DB_USER');
-
-// Read the password file path from an environment variable
-$password_file_path = getenv('PASSWORD_FILE_PATH');
-
-// Read the password from the file
-$db_pass = trim(file_get_contents($password_file_path));
+$db_pass = getenv('DB_PASS');
 
 // Create a new PDO instance
 $db_handle = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
