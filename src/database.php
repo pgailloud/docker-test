3b12260d32c656c0713a5b6b4467afaadc3261c3
Pascal Gailloud <pgailloud@bluewin.ch>
2024-10-01 09:07:51 +0200

    Mode debug

--- src/database.php
+++ src/database.php
@@ -8,6 +8,11 @@ $db_name = getenv('DB_NAME');
 $db_user = getenv('DB_USER');
 $db_pass = getenv('DB_PASS');
 
+echo "DB_HOST -> $db_host";
+echo "DB_NAME -> $db_name";
+echo "DB_USER -> $db_user";
+echo "DB_PASS -> $db_pass";
+
 // Create a new PDO instance
 $db_handle = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 
